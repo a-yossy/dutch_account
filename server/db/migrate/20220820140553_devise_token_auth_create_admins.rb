097655c45d83c7fe4c03bc4e0263b5a99e8d2d48
atsuya02 <atsuya0930@yahoo.co.jp>
2022-08-20 23:59:01 +0900

    feat: migrationファイル修正後migrate実行

--- server/db/migrate/20220820140553_devise_token_auth_create_admins.rb
+++ server/db/migrate/20220820140553_devise_token_auth_create_admins.rb
@@ -10,18 +10,18 @@ class DeviseTokenAuthCreateAdmins < ActiveRecord::Migration[7.0]
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
+      # t.boolean  :allow_password_change, :default => false
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
       ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
@@ -29,21 +29,21 @@ class DeviseTokenAuthCreateAdmins < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
-      t.string :email
+      t.string :name, null: false
+      # t.string :nickname
+      # t.string :image
+      t.string :email, null: false
 
       ## Tokens
-      t.text :tokens
+      # t.text :tokens
 
       t.timestamps
     end
 
     add_index :admins, :email,                unique: true
     add_index :admins, [:uid, :provider],     unique: true
-    add_index :admins, :reset_password_token, unique: true
-    add_index :admins, :confirmation_token,   unique: true
+    # add_index :admins, :reset_password_token, unique: true
+    # add_index :admins, :confirmation_token,   unique: true
     # add_index :admins, :unlock_token,         unique: true
   end
 end

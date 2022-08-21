8cced686aa1e45a1a7c298da51cf2aaa6271fd97
atsuya02 <atsuya0930@yahoo.co.jp>
2022-08-21 23:02:12 +0900

    feat: Adminモデルの設定修正

--- server/app/models/admin.rb
+++ server/app/models/admin.rb
@@ -4,6 +4,8 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, presence: true
 end

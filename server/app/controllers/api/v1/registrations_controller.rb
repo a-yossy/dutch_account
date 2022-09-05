4c315a37bb252fdd9997e507ef21014284a4c91b
atsuya02 <atsuya0930@yahoo.co.jp>
2022-09-05 22:06:25 +0900

    feat: devise_token_authのレスポンス修正

--- server/app/controllers/api/v1/registrations_controller.rb
+++ server/app/controllers/api/v1/registrations_controller.rb
@@ -5,6 +5,20 @@ class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsControlle
 
   protected
 
+  def render_create_success
+    data = resource_data
+    render json: {
+      id: data['id'],
+      name: data['name']
+    }
+  end
+
+  def render_create_error
+    render json: {
+      messages: resource_errors[:full_messages]
+    }, status: :unprocessable_entity
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
   end
--- server/app/controllers/api/v1/sessions_controller.rb
+++ server/app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
 class Api::V1::SessionsController < DeviseTokenAuth::SessionsController
+  protected
+
+  def render_create_success
+    data = resource_data(resource_json: @resource.token_validation_response)
+    render json: {
+      id: data['id'],
+      name: data['name']
+    }
+  end
+
+  def render_create_error_bad_credentials
+    render json: {
+      messages: [I18n.t('devise_token_auth.sessions.bad_credentials')]
+    }, status: :unauthorized
+  end
+
+  def render_destroy_success
+    render status: :ok
+  end
+
+  def render_destroy_error
+    render json: {
+      messages: [I18n.t('devise_token_auth.sessions.user_not_found')]
+    }, status: :not_found
+  end
 end

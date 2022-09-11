b0675865e3c4d484c597c22561ffb6ec98bc500c
atsuya02 <atsuya0930@yahoo.co.jp>
2022-09-11 18:04:13 +0900

    feat: 不要なinclude削除

--- server/spec/support/devise_token_auth.rb
+++ server/spec/support/devise_token_auth.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module DeviseTokenAuth
-  include ApiMacros
-
   def sign_in(user)
     post_as_json api_v1_sign_in_path, { email: user.email, password: user.password }
     response.headers.slice('client', 'access-token', 'uid')

dab6c601f1722e23ec3df9d4b1b3b754f71e2420
atsuya02 <atsuya0930@yahoo.co.jp>
2022-09-17 00:58:24 +0900

    feat: Linkコンポーネント作成

---
+++ client/components/Link.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react"
+import NextLink from "next/link"
+import { Link as ChakraLink } from "@chakra-ui/react"
+
+type LinkProps = Required<{
+  readonly href: string
+  readonly ml: number,
+  readonly fontSize: number,
+  readonly fontWeight: string,
+  readonly title: string
+}>
+
+const Link: FC<LinkProps> = ({ href, ml, fontSize, fontWeight, title }) => (
+  <NextLink href={href} passHref>
+    <ChakraLink ml={ml} fontSize={fontSize} fontWeight={fontWeight} style={{ textDecoration: 'none' }}>
+      {title}
+    </ChakraLink>
+  </NextLink>
+)
+
+export default Link

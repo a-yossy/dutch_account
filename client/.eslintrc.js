1507d8977ac74a82a740e3717782f8870d4edc6e
atsuya02 <atsuya0930@yahoo.co.jp>
2022-09-19 00:42:17 +0900

    feat: eslint設定

---
+++ client/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'next/core-web-vitals',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+};

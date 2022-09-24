10a555c7d6944f231ff7eabb2bbaef8f19b7e182
atsuya02 <atsuya0930@yahoo.co.jp>
2022-09-24 19:00:59 +0900

    feat: yarn format

--- client/src/formSchemas/responseErrorSchema.ts
+++ client/src/formSchemas/responseErrorSchema.ts
@@ -1,10 +1,7 @@
 import { z } from 'zod';
 import { ResponseError } from 'openapi-generator/api/model';
 
-const ResponseErrorSchema: z.ZodType<ResponseError> =
-  z.object({
-    messages: z.string().array().nonempty(),
-  })
-;
-
+const ResponseErrorSchema: z.ZodType<ResponseError> = z.object({
+  messages: z.string().array().nonempty(),
+});
 export default ResponseErrorSchema;
--- client/src/formSchemas/signUpSchema.ts
+++ client/src/formSchemas/signUpSchema.ts
@@ -5,18 +5,16 @@ export type SignUpForm = {
   password_confirmation: string;
 } & SignUpRequest;
 
-export const SignUpSchema: z.ZodType<SignUpForm> =
-  z
-    .object({
-      name: z.string().min(1, { message: '名前を入力してください' }),
-      email: z.string().email({ message: 'メールアドレスが不正です' }),
-      password: z
-        .string()
-        .min(6, { message: 'パスワードは6文字以上で入力してください' }),
-      password_confirmation: z.string(),
-    })
-    .refine((data) => data.password === data.password_confirmation, {
-      message: 'パスワードと確認用パスワードが一致しません',
-      path: ['password_confirmation'],
-    })
-;
+export const SignUpSchema: z.ZodType<SignUpForm> = z
+  .object({
+    name: z.string().min(1, { message: '名前を入力してください' }),
+    email: z.string().email({ message: 'メールアドレスが不正です' }),
+    password: z
+      .string()
+      .min(6, { message: 'パスワードは6文字以上で入力してください' }),
+    password_confirmation: z.string(),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: 'パスワードと確認用パスワードが一致しません',
+    path: ['password_confirmation'],
+  });

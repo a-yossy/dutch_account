ed1e47008670749e09f656685a02a5bf9cbdb489
atsuya02 <atsuya0930@yahoo.co.jp>
2022-09-29 01:50:36 +0900

    feat: 任意のプロパティを必須にする型作成

--- client/src/libs/isResponseError.ts
+++ client/src/libs/isResponseError.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosError } from 'axios';
 import { ResponseError } from 'openapi-generator/api';
 import ResponseErrorSchema from 'src/types/responseErrorSchema';
+import SomeRequired from 'src/types/someRequired';
 
 const isResponseError = (
   error: unknown
-): error is Required<AxiosError<ResponseError>> => {
+): error is SomeRequired<AxiosError<ResponseError>, 'response'> => {
   if (axios.isAxiosError(error) && error.response !== undefined) {
     const responseError = ResponseErrorSchema.safeParse(error.response.data);
 

fb9d1ff1bf1718f53616bd6d72b75d42990d07a5
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-11 23:05:11 +0900

    test: 管理グループ取得APIテスト作成

--- server/spec/requests/api/v1/management_groups_controller_spec.rb
+++ server/spec/requests/api/v1/management_groups_controller_spec.rb
@@ -22,4 +22,37 @@ RSpec.describe Api::V1::ManagementGroupsController, type: :request do
       end
     end
   end
+
+  describe '#show' do
+    let(:user) { create(:user) }
+    let(:auth_tokens) { sign_in(user) }
+    let(:management_group) { create(:management_group) }
+
+    context 'when the user signs in' do
+      context 'when the management group related to the user does not exist' do
+        let(:other_management_group) { create(:management_group) }
+
+        it 'returns not_found response' do
+          get api_v1_management_group_path(other_management_group), headers: auth_tokens
+          assert_response_schema_confirm(404)
+        end
+      end
+
+      context 'when the management group related to the user exists' do
+        before { create(:management_affiliation, user:, management_group:) }
+
+        it 'returns success response' do
+          get api_v1_management_group_path(management_group), headers: auth_tokens
+          assert_response_schema_confirm(200)
+        end
+      end
+    end
+
+    context 'when the user does not sign in' do
+      it 'returns unauthorized response' do
+        get api_v1_management_group_path(management_group)
+        assert_response_schema_confirm(401)
+      end
+    end
+  end
 end

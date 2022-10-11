a516b1db61e714b61f3962b82b63fd1d330d8da0
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-11 23:04:46 +0900

    feat: APIの404エラー処理作成

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -3,8 +3,15 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include DeviseHackFakeSession
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
 
   private
+
+  def render_not_found_error
+    render json: {
+      messages: [I18n.t('errors.messages.not_found')]
+    }, status: :not_found
+  end
 
   def render_authenticate_error
     # rubocop:disable Style/RedundantReturn

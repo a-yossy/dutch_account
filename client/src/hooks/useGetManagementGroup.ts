9a5936659f55cd9372e033e11bbb5a3da2558327
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-23 01:09:43 +0900

    fix

--- client/src/hooks/useGetManagementGroup.ts
+++ client/src/hooks/useGetManagementGroup.ts
@@ -1,18 +1,22 @@
+import type { NextRouter } from 'next/router';
 import { getAuthCookies } from 'src/libs/getAuthCookies';
 import { ManagementGroup, ManagementGroupApi } from 'src/openapi-generator';
 import { AxiosResponseError } from 'src/types/axiosResponseError';
 import useSWR from 'swr';
 
-export const useGetManagementGroup = (id: string) => {
-  const fetcher = () =>
-    new ManagementGroupApi()
-      .getManagementGroupByManagementGroupId(id, {
-        headers: getAuthCookies(),
-      })
-      .then((res) => res.data);
+export const useGetManagementGroup = (id: NextRouter['query']['id']) => {
+  const fetcher =
+    typeof id === 'string'
+      ? () =>
+          new ManagementGroupApi()
+            .getManagementGroupByManagementGroupId(id, {
+              headers: getAuthCookies(),
+            })
+            .then((res) => res.data)
+      : null;
 
   const { data, error } = useSWR<ManagementGroup, AxiosResponseError>(
-    `api/v1/management_groups/${id}`,
+    typeof id === 'string' ? `api/v1/management_groups/${id}` : null,
     fetcher
   );
 
--- client/src/pages/management_groups/[id].tsx
+++ client/src/pages/management_groups/[id].tsx
@@ -1,14 +1,23 @@
-import { Spinner } from '@chakra-ui/react';
+import { Spinner, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { ManagementGroupWithId } from 'src/components/ManagementGroupWithId';
+import { useGetManagementGroup } from 'src/hooks/useGetManagementGroup';
+import { isResponseError } from 'src/libs/isResponseError';
+import NotFoundErrorPage from 'src/pages/404';
 
 const ManagementGroup = () => {
   const router = useRouter();
   const { id } = router.query;
+  const { managementGroup, error } = useGetManagementGroup(id);
 
-  if (typeof id !== 'string') return <Spinner />;
+  if (isResponseError(error) && error.response.status === 404)
+    return <NotFoundErrorPage />;
+  if (!managementGroup) return <Spinner />;
 
-  return <ManagementGroupWithId id={id} />;
+  return (
+    <Text fontSize='xl' align='center'>
+      {managementGroup.name}
+    </Text>
+  );
 };
 
 export default ManagementGroup;

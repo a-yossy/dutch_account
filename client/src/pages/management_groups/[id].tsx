056d99ebe14719944488b6c72eee8e111f43ed34
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-23 16:43:09 +0900

    refactor: ManagementGroupWithIdコンポーネントを[id].tsxに移動

--- client/src/pages/management_groups/[id].tsx
+++ client/src/pages/management_groups/[id].tsx
@@ -1,6 +1,27 @@
-import { Spinner } from '@chakra-ui/react';
+import { FC } from 'react';
 import { useRouter } from 'next/router';
-import { ManagementGroupWithId } from 'src/components/ManagementGroupWithId';
+import { Spinner, Text } from '@chakra-ui/react';
+import { useGetManagementGroup } from 'src/hooks/useGetManagementGroup';
+import { isResponseError } from 'src/libs/isResponseError';
+import NotFoundErrorPage from 'src/pages/404';
+
+type ManagementGroupWithIdProps = {
+  id: string;
+};
+
+const ManagementGroupWithId: FC<ManagementGroupWithIdProps> = ({ id }) => {
+  const { managementGroup, error } = useGetManagementGroup(id);
+
+  if (isResponseError(error) && error.response.status === 404)
+    return <NotFoundErrorPage />;
+  if (!managementGroup) return <Spinner />;
+
+  return (
+    <Text fontSize='xl' align='center'>
+      {managementGroup.name}
+    </Text>
+  );
+};
 
 const ManagementGroup = () => {
   const router = useRouter();

758a2d38004c9b7b5049254aabe07b7231bcdcca
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-24 00:34:49 +0900

    feat: 管理者詳細ページにユーザー表示

--- client/src/pages/management_groups/[id].tsx
+++ client/src/pages/management_groups/[id].tsx
@@ -1,9 +1,47 @@
 import { FC } from 'react';
 import { useRouter } from 'next/router';
-import { Spinner, Text } from '@chakra-ui/react';
+import { Spinner, Text, Box } from '@chakra-ui/react';
 import { useGetManagementGroup } from 'src/hooks/useGetManagementGroup';
 import { isResponseError } from 'src/libs/isResponseError';
 import NotFoundErrorPage from 'src/pages/404';
+import { useGetManagementAffiliationUsers } from 'src/hooks/useGetManagementAffiliationUsers';
+import { ManagementGroup } from 'src/openapi-generator';
+
+type ManagementAffiliationUsersWithManagementGroupProps = {
+  managementGroup: ManagementGroup;
+};
+
+const ManagementAffiliationUsersWithManagementGroup: FC<
+  ManagementAffiliationUsersWithManagementGroupProps
+> = ({ managementGroup }) => {
+  const { managementAffiliationUsers, error } =
+    useGetManagementAffiliationUsers(managementGroup.id.toString());
+  if (isResponseError(error) && error.response.status === 404)
+    return <NotFoundErrorPage />;
+  if (!managementAffiliationUsers) return <Spinner />;
+
+  return (
+    <>
+      {managementAffiliationUsers.map((managementAffiliationUser) => (
+        <Box
+          key={managementAffiliationUser.id}
+          width={400}
+          mx='auto'
+          boxShadow='dark-lg'
+          rounded='md'
+          bg='#164b9f1b'
+          height={12}
+          display='flex'
+          alignItems='center'
+          pl={3}
+          mt={5}
+        >
+          {managementAffiliationUser.name}
+        </Box>
+      ))}
+    </>
+  );
+};
 
 type ManagementGroupWithIdProps = {
   id: string;
@@ -17,13 +55,18 @@ const ManagementGroupWithId: FC<ManagementGroupWithIdProps> = ({ id }) => {
   if (!managementGroup) return <Spinner />;
 
   return (
-    <Text fontSize='xl' align='center'>
-      {managementGroup.name}
-    </Text>
+    <>
+      <Text fontSize='xl' align='center'>
+        管理グループ：{managementGroup.name}
+      </Text>
+      <ManagementAffiliationUsersWithManagementGroup
+        managementGroup={managementGroup}
+      />
+    </>
   );
 };
 
-const ManagementGroup = () => {
+const ManagementGroupPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
@@ -32,4 +75,4 @@ const ManagementGroup = () => {
   return <ManagementGroupWithId id={id} />;
 };
 
-export default ManagementGroup;
+export default ManagementGroupPage;

b9e7e848fd0d27fd0b4fb6c3753006925d84bd97
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-24 12:10:38 +0900

    feat: 条件付きフェッチを使うよう修正

--- client/src/pages/management_groups/[id].tsx
+++ client/src/pages/management_groups/[id].tsx
@@ -5,7 +5,6 @@ import { useGetManagementGroup } from 'src/hooks/useGetManagementGroup';
 import { isResponseError } from 'src/libs/isResponseError';
 import NotFoundErrorPage from 'src/pages/404';
 import { useGetManagementAffiliationUsers } from 'src/hooks/useGetManagementAffiliationUsers';
-import { ManagementGroup as ManagementGroupType } from 'src/openapi-generator';
 
 const ManagementGroupPage = () => {
   const router = useRouter();
@@ -23,10 +22,19 @@ type ManagementGroupProps = {
 };
 
 const ManagementGroup: FC<ManagementGroupProps> = ({ id }) => {
-  const { managementGroup, error } = useGetManagementGroup(id);
+  const { managementGroup, error: managementGroupError } =
+    useGetManagementGroup(id);
+  const { managementAffiliationUsers, error: managementAffiliationUsersError } =
+    useGetManagementAffiliationUsers(managementGroup?.id.toString());
 
-  if (isResponseError(error) && error.response.status === 404)
+  if (
+    (isResponseError(managementGroupError) &&
+      managementGroupError.response.status === 404) ||
+    (isResponseError(managementAffiliationUsersError) &&
+      managementAffiliationUsersError.response.status === 404)
+  )
     return <NotFoundErrorPage />;
+
   if (!managementGroup) return <Spinner />;
 
   return (
@@ -34,43 +42,27 @@ const ManagementGroup: FC<ManagementGroupProps> = ({ id }) => {
       <Text fontSize='xl' align='center'>
         管理グループ：{managementGroup.name}
       </Text>
-      <ManagementAffiliationUsers managementGroup={managementGroup} />
-    </>
-  );
-};
-
-type ManagementAffiliationUsersProps = {
-  managementGroup: ManagementGroupType;
-};
-
-const ManagementAffiliationUsers: FC<ManagementAffiliationUsersProps> = ({
-  managementGroup,
-}) => {
-  const { managementAffiliationUsers, error } =
-    useGetManagementAffiliationUsers(managementGroup.id.toString());
-  if (isResponseError(error) && error.response.status === 404)
-    return <NotFoundErrorPage />;
-  if (!managementAffiliationUsers) return <Spinner />;
-
-  return (
-    <>
-      {managementAffiliationUsers.map((managementAffiliationUser) => (
-        <Box
-          key={managementAffiliationUser.id}
-          width={400}
-          mx='auto'
-          boxShadow='dark-lg'
-          rounded='md'
-          bg='#164b9f1b'
-          height={12}
-          display='flex'
-          alignItems='center'
-          pl={3}
-          mt={5}
-        >
-          {managementAffiliationUser.name}
-        </Box>
-      ))}
+      {managementAffiliationUsers === undefined ? (
+        <Spinner />
+      ) : (
+        managementAffiliationUsers.map((managementAffiliationUser) => (
+          <Box
+            key={managementAffiliationUser.id}
+            width={400}
+            mx='auto'
+            boxShadow='dark-lg'
+            rounded='md'
+            bg='#164b9f1b'
+            height={12}
+            display='flex'
+            alignItems='center'
+            pl={3}
+            mt={5}
+          >
+            {managementAffiliationUser.name}
+          </Box>
+        ))
+      )}
     </>
   );
 };

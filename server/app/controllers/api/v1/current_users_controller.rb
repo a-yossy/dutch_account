6130d80708a8024d85dae012bf8b4c9789335a1f
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-24 17:09:56 +0900

    feat: alba使用

--- server/app/controllers/api/v1/current_users_controller.rb
+++ server/app/controllers/api/v1/current_users_controller.rb
@@ -4,9 +4,6 @@ class Api::V1::CurrentUsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render json: {
-      id: current_user.id,
-      name: current_user.name
-    }
+    render json: UserResource.new(current_user).serialize
   end
 end
--- server/app/controllers/api/v1/management_groups/users_controller.rb
+++ server/app/controllers/api/v1/management_groups/users_controller.rb
@@ -4,10 +4,11 @@ class Api::V1::ManagementGroups::UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: current_user.management_groups
-                             .find(params[:management_group_id])
-                             .users
-                             .select('id', 'name')
-                             .alphabetical_order
+    render json: UserResource.new(
+      current_user.management_groups
+      .find(params[:management_group_id])
+      .users
+      .alphabetical_order
+    ).serialize
   end
 end

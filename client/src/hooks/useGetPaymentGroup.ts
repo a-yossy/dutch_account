05a00c5223ce4043f8bba79c9d440fc91f890d04
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-27 01:46:38 +0900

    feat: 支払グループ詳細ページ作成

---
+++ client/src/hooks/useGetPaymentGroup.ts
@@ -0,0 +1,23 @@
+import useSWR from 'swr';
+import { getAuthCookies } from 'src/libs/getAuthCookies';
+import { PaymentGroup, PaymentGroupApi } from 'src/openapi-generator';
+import { AxiosResponseError } from 'src/types/axiosResponseError';
+
+export const useGetPaymentGroup = (
+  managementGroupId: string,
+  paymentGroupId: string
+) => {
+  const fetcher = () =>
+    new PaymentGroupApi()
+      .getPaymentGroupByPaymentGroupId(managementGroupId, paymentGroupId, {
+        headers: getAuthCookies(),
+      })
+      .then((res) => res.data);
+
+  const { data, error } = useSWR<PaymentGroup, AxiosResponseError>(
+    `api/v1/management_groups/${managementGroupId}/payment_groups/${paymentGroupId}`,
+    fetcher
+  );
+
+  return { paymentGroup: data, error };
+};

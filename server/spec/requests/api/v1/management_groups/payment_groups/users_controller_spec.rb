0378cebf59815ef1736cde23b3ac8cdfa5e50d0c
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-28 00:12:32 +0900

    test: 支払グループ全所属ユーザー取得APIテスト作成

---
+++ server/spec/requests/api/v1/management_groups/payment_groups/users_controller_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::ManagementGroups::PaymentGroups::UsersController, type: :request do
+  describe '#index' do
+    let(:management_group) { create(:management_group) }
+    let(:payment_group) { create(:payment_group) }
+
+    context 'when the user logs in' do
+      let(:user) { create(:user) }
+      let(:auth_tokens) { log_in(user) }
+
+      context 'when the management group related to the user does not exit' do
+        it 'returns not_found response' do
+          get api_v1_management_group_payment_group_users_path(management_group, payment_group), headers: auth_tokens
+          assert_response_schema_confirm(404)
+        end
+      end
+
+      context 'when the management group related to the user exists' do
+        before { create(:management_affiliation, user:, management_group:) }
+
+        context 'when the payment group related to the management group does not exist' do
+          it 'returns not_found response' do
+            get api_v1_management_group_payment_group_users_path(management_group, payment_group), headers: auth_tokens
+            assert_response_schema_confirm(404)
+          end
+        end
+
+        context 'when the payment group related to the management group exists' do
+          let(:payment_group) { create(:payment_group, management_group:) }
+
+          it 'returns success response' do
+            get api_v1_management_group_payment_group_users_path(management_group, payment_group), headers: auth_tokens
+            assert_response_schema_confirm(200)
+          end
+        end
+      end
+    end
+
+    context 'when the user does not log in' do
+      it 'returns unauthorized response' do
+        get api_v1_management_group_payment_group_users_path(management_group, payment_group)
+        assert_response_schema_confirm(401)
+      end
+    end
+  end
+end

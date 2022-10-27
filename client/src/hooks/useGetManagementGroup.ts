e376625bc0ed8be8d2783963f7c515efc109cbf3
atsuya02 <atsuya0930@yahoo.co.jp>
2022-10-27 20:53:27 +0900

    fix

--- client/src/hooks/useGetManagementGroup.ts
+++ client/src/hooks/useGetManagementGroup.ts
@@ -10,6 +10,7 @@ export const useGetManagementGroup = (id: string) => {
         headers: getAuthCookies(),
       })
       .then((res) => res.data);
+
   const { data, error } = useSWR<ManagementGroup, AxiosResponseError>(
     `api/v1/management_groups/${id}`,
     fetcher

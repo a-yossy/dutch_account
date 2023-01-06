02ae6e30fabf70c9d311f9db7176b90c69677abe
a-yossy <atsuya0930@yahoo.co.jp>
2023-01-06 15:32:01 +0900

    feat: storybookにmsw追加

--- client/src/features/management_groups/components/ManagementGroupTab.stories.tsx
+++ client/src/features/management_groups/components/ManagementGroupTab.stories.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ManagementGroupTab } from 'src/features/management_groups/components/ManagementGroupTab';
+import { getManagementGroupUsersHandler } from 'src/test/server/handlers/managementGroup';
+import { getManagementGroupPaymentGroupsHandler } from 'src/test/server/handlers/paymentGroup';
 
 export default {
   component: ManagementGroupTab,
+  parameters: {
+    msw: {
+      handlers: [
+        getManagementGroupUsersHandler(),
+        getManagementGroupPaymentGroupsHandler(),
+      ],
+    },
+  },
 } as ComponentMeta<typeof ManagementGroupTab>;
 
 const Template: ComponentStory<typeof ManagementGroupTab> = (args) => (

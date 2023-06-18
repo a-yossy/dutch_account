37ab68865abe6c9d34c152213cc36fdeda62f870
a-yossy <atsuya0930@yahoo.co.jp>
2023-06-18 15:13:16 +0900

    fix: ログアウト時にrecoilのstateも変更するよう修正

--- client/src/components/layouts/Header/hooks/index.ts
+++ client/src/components/layouts/Header/hooks/index.ts
@@ -5,16 +5,19 @@ import { getAuthCookies } from 'src/libs/nookies/getAuthCookies';
 import { isResponseError } from 'src/libs/isResponseError';
 import { useToast } from 'src/hooks/useToast';
 import { destroyAuthCookies } from 'src/libs/nookies/destroyAuthCookies';
+import { useSetCurrentUser } from 'src/recoil/currentUserState';
 
 export const useLogOut = () => {
   const toast = useToast();
   const router = useRouter();
+  const setCurrentUser = useSetCurrentUser();
   const logOut = useCallback(async () => {
     try {
       await new UserApi().logOut({ headers: getAuthCookies() });
       destroyAuthCookies();
       await router.push('/');
       toast('success', 'ログアウトしました');
+      setCurrentUser({ state: 'log_out' });
     } catch (error: unknown) {
       if (isResponseError(error)) {
         toast(
@@ -24,7 +27,7 @@ export const useLogOut = () => {
         );
       }
     }
-  }, [router, toast]);
+  }, [router, setCurrentUser, toast]);
 
   return logOut;
 };

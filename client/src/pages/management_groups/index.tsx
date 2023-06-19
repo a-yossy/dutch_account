1da20c438b1c506642ee5baa39104cbbe368a88b
a-yossy <atsuya0930@yahoo.co.jp>
2023-06-19 13:05:31 +0900

    refactor: 不要なファイル削除

--- client/src/pages/management_groups/index.tsx
+++
@@ -1,6 +0,0 @@
-import type { NextPage } from 'next';
-import { ManagementGroups } from 'src/features/management_groups';
-
-const ManagementGroupsPage: NextPage = () => <ManagementGroups />;
-
-export default ManagementGroupsPage;

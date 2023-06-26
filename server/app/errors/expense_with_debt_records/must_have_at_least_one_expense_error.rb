09ba25a37197e92285a9ca2005f911b63e616f0e
a-yossy <atsuya0930@yahoo.co.jp>
2023-06-26 21:55:48 +0900

    feat: ExpenseWithDebtRecordsCreatorクラス関連作成

---
+++ server/app/errors/expense_with_debt_records/must_have_at_least_one_expense_error.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ExpenseWithDebtRecords
+  class MustHaveAtLeastOneExpenseError < StandardError; end
+end
--- server/app/models/debt_record.rb
+++ server/app/models/debt_record.rb
@@ -4,4 +4,7 @@ class DebtRecord < ApplicationRecord
   belongs_to :lending_user, class_name: 'User'
   belongs_to :borrowing_user, class_name: 'User'
   belongs_to :expense
+
+  validates :amount_of_money, presence: true, numericality: { greater_than: 0 }
+  validates :lending_user, uniqueness: { scope: %i[borrowing_user expense] }
 end

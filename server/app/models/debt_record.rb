667d600eb5b5e72012a92b43eb80aa3b17aeeab9
a-yossy <atsuya0930@yahoo.co.jp>
2023-06-30 01:04:37 +0900

    feat: debt_recordにvalidate追加

--- server/app/models/debt_record.rb
+++ server/app/models/debt_record.rb
@@ -7,4 +7,27 @@ class DebtRecord < ApplicationRecord
 
   validates :amount_of_money, presence: true, numericality: { greater_than: 0 }
   validates :borrowing_user, uniqueness: { scope: :expense }
+  validate :check_lending_user_and_borrowing_user_are_different, if: %i[lending_user borrowing_user]
+  validate :check_borrowing_user_belongs_to_payment_group, if: %i[borrowing_user expense]
+  validate :check_lending_user_is_same_as_expense_user, if: %i[lending_user expense]
+
+  private
+
+  def check_lending_user_and_borrowing_user_are_different
+    return unless lending_user.id == borrowing_user.id
+
+    errors.add(:borrowing_user, "は#{DebtRecord.human_attribute_name('lending_user')}と同じユーザーにできません")
+  end
+
+  def check_borrowing_user_belongs_to_payment_group
+    return if expense.payment_group.payment_affiliations.pluck(:user_id).include?(borrowing_user.id)
+
+    errors.add(:borrowing_user, "は#{PaymentGroup.model_name.human}に所属していません")
+  end
+
+  def check_lending_user_is_same_as_expense_user
+    return if expense.user_id == lending_user.id
+
+    errors.add(:lending_user, 'は支払ったユーザーと同じユーザーにしてください')
+  end
 end

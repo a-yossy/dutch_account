79a01b65047273444bd7ec5ea2d87b75916efd18
a-yossy <atsuya0930@yahoo.co.jp>
2023-07-07 00:04:21 +0900

    feat: storybook修正

---
+++ client/src/__fixtures__/totalBorrowingAndLending.ts
@@ -0,0 +1,13 @@
+import { TotalBorrowingAndLending } from 'src/openapi-generator';
+
+export const getManagementGroupTotalBorrowingAndLendingsResponse: TotalBorrowingAndLending[] =
+  [
+    {
+      user_id: '1',
+      amount_of_money: 1000,
+    },
+    {
+      user_id: '2',
+      amount_of_money: -1000,
+    },
+  ];

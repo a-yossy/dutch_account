2eb419227073d367b2042f6cc8d902718e477e66
a-yossy <atsuya0930@yahoo.co.jp>
2023-07-19 01:58:01 +0900

    feat: buttonにrefを渡せるよう修正

--- client/src/components/elements/Button/OutlineButton.tsx
+++ client/src/components/elements/Button/OutlineButton.tsx
@@ -2,13 +2,16 @@ import { FC } from 'react';
 import { Button, ButtonProps } from '@chakra-ui/react';
 import { OmitStrict } from 'src/types/omitStrict';
 
-type OutlineButtonProps = OmitStrict<ButtonProps, 'variant'>;
+type OutlineButtonProps = OmitStrict<ButtonProps, 'variant'> & {
+  ref?: React.Ref<HTMLButtonElement>;
+};
 
 export const OutlineButton: FC<OutlineButtonProps> = ({
   children,
+  ref,
   ...props
 }) => (
-  <Button variant='outline' {...props}>
+  <Button variant='outline' {...props} ref={ref}>
     {children}
   </Button>
 );

2af166136ae9399d63b02aa80be154b5409dedc9
a-yossy <atsuya0930@yahoo.co.jp>
2023-07-20 01:04:54 +0900

    fix

--- server/app/services/debt_records_payment_completer.rb
+++ server/app/services/debt_records_payment_completer.rb
@@ -6,13 +6,13 @@ class DebtRecordsPaymentCompleter
   end
 
   def call!
-    check_exist_at_least_unpaid_debt_record!
+    check_exist_at_least_one_unpaid_debt_record!
     @unpaid_debt_records.update(is_paid: true)
   end
 
   private
 
-  def check_exist_at_least_unpaid_debt_record!
+  def check_exist_at_least_one_unpaid_debt_record!
     return if @unpaid_debt_records.size >= 1
 
     raise DebtRecords::NotExistUnpaidDebtRecordsError, "未払いの#{DebtRecord.model_name.human}が存在しません"
